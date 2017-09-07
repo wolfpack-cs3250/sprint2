e2e2eb3c3b5bd1a0973300d337e968096ba4b18a
hexphase <13786214+hexphase@users.noreply.github.com>
2017-09-07 10:35:02 -0600

    added spring initialized project with Gradle

---
+++ banner-project/src/main/java/com/example/bannerproject/BannerProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.bannerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BannerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BannerProjectApplication.class, args);
+	}
+}
