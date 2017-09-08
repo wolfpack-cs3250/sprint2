8e54b7dedf166b82da5beb40856deb01ff928ed1
JasonTheMain <jason.gambino3@gmail.com>
2017-09-08 10:46:08 -0600

    Add files via upload

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package hello;
+
+/**
+ * Created by Jason Gambino on 9/8/2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
