8f2ad1e682431fa9f8851e01311b45f4fb2e4246
sal <sal2993@hotmail.com>
2017-09-14 15:05:02 -0600

    Got a basic header/footer template to work AND got first add controller to work.

--- src/main/java/com/banner/bannerApplication/controllers/UserController.java
+++ src/main/java/com/banner/bannerApplication/controllers/UserController.java
@@ -7,17 +7,39 @@ package com.banner.bannerApplication.controllers;
 */
 
 import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
+import com.banner.bannerApplication.entities.User;
+import com.banner.bannerApplication.repositories.UserRepository;
 
+@Controller
+@RequestMapping("userpage")
 public class UserController {
-    @RequestMapping("product/{id}")
-    public String showProduct(@PathVariable Integer id) {
-        return "index";
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping(path="/add")
+    public @ResponseBody String addNewUser (@RequestParam String firstname,
+                                            @RequestParam String lastname) {
+        User n = new User();
+        n.setFirstName(firstname);
+        n.setLastName(lastname);
+        userRepository.save(n);
+        return "Saved";
     }
 
+    /*
+    @GetMapping(path="/")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name );
+        return "userpage";
+    }
+
+//    @RequestMapping(method=POST)
+    public String greeting(@RequestParam(value="firstname", required=true) String name);
+
+    */
 }
